b347ed37323f166e051b3429dc41e1722ed349fa
HamzaMeski <meskihamza5@gmail.com>
2024-11-23 00:21:50 +0100

    first harvest management setup

--- backend/src/main/java/com/citronix/backend/repository/HarvestRepository.java
+++ backend/src/main/java/com/citronix/backend/repository/HarvestRepository.java
@@ -1,9 +1,12 @@
 package com.citronix.backend.repository;
 
 import com.citronix.backend.entity.Harvest;
+import com.citronix.backend.util.constants.Season;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
 
 @Repository
 public interface HarvestRepository extends JpaRepository<Harvest, Long> {
-} 
+    boolean existsBySeasonAndHarvestDateBetween(Season season, LocalDate startDate, LocalDate endDate);
+}
